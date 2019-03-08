60a43def9eb788c60643490f7a67ae2144728bb8
Sergio Miracco <sergio.miracco@gmail.com>
2019-03-08 08:02:25 +0100

    add options menu

--- app/src/main/java/ch/semi/ledc/MainActivity.kt
+++ app/src/main/java/ch/semi/ledc/MainActivity.kt
@@ -2,11 +2,25 @@ package ch.semi.ledc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater = menuInflater
+        //inflater.inflate(, menu)
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+
 }
