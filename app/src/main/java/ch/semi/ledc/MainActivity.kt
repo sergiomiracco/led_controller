7b4434882b07db8dcd844fabcdf742e829a2b5a5
Sergio Miracco <sergio.miracco@gmail.com>
2019-03-19 22:07:14 +0100

    declare first functional set of vars

--- app/src/main/java/ch/semi/ledc/MainActivity.kt
+++ app/src/main/java/ch/semi/ledc/MainActivity.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +17,14 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val _channels: RadioGroup = channel_radio_group
+        val _chInside: RadioButton = inside_radio
+        val _chOutside: RadioButton = outside_radio
+        val _chAll: RadioButton = all_radio
+
+        val _colorCircle: ImageView = rgb_color_circle
+
+        val _displayAll: View = color_display_all
 
     }
 
