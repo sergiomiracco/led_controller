d917ef448b3e0efc8b392cf744c89c288b6a94ac
Sergio Miracco <sergio.miracco@gmail.com>
2019-04-11 23:05:22 +0200

    start with settings fragment

---
+++ app/src/main/java/ch/semi/ledc/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package ch.semi.ledc
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle, rootKey: String) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+
+}
